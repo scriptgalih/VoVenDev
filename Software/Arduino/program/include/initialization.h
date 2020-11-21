3f7469c295bc83820bc8c06d6da48b3ab0cc6cc1
Galih Setyawan <galih.setyawan@mail.ugm.ac.id>
2020-11-21 17:33:26 +0700

    b

---
+++ Software/Arduino/program/include/initialization.h
@@ -0,0 +1,41 @@
+#define LIMITSWITCH_R A6
+#define LIMITSWITCH_L A4
+#define LED A2
+#define RX1 19
+#define TX1 18
+#define SDL 21
+#define SDA 20
+
+#define PULSE_PIN 37
+#define DIR_PIN   35
+#define EN_PIN    33
+
+#define TIMER1 1
+#define TIMER3 3
+
+#define FLOWPIN     A8
+#define PRESSUREPIN A9
+#define OXYGENPIN   A10
+
+int inspiration = 1;
+int expiration = 2;
+
+int breath_frequency = 10;
+
+unsigned long set_OCR1, set_OCR3;
+
+int step_count = 0;
+
+int phase = 0;
+
+bool MACHNE_ON = false;
+bool change_phase = false;
+bool SENDDATA = false;
+
+int mult_timer3=0;
+
+String serial_text;
+
+float offset_pressure;
+
+float disp_volume=1, disp_oxygen=2, disp_flow=3;
---
+++ Software/Arduino/program/src/main.cpp
@@ -0,0 +1,353 @@
+#include <Arduino.h>
+#include <Thread.h>
+#include <ThreadController.h>
+#include <ArduinoJson.h>
+#include "RTClib.h"
+#include "initialization.h"
+#include "function.h"
+RTC_DS1307 rtc;
+
+Thread phaseThread = Thread();
+Thread sendData = Thread();
+Thread sendStaticData = Thread();
+
+class SensorThread : public Thread
+{
+public:
+  int current_input;
+  int pin;
+  float current_output, alpha, previous_output;
+  // No, "run" cannot be anything...
+  // Because Thread uses the method "run" to run threads,
+  // we MUST overload this method here. using anything other
+  // than "run" will not work properly...
+  void run()
+  {
+    // Reads the analog pin, and saves it localy
+    current_input = analogRead(pin);
+    current_output = alpha * current_input + (1 - alpha) * previous_output;
+    previous_output = current_output;
+    runned();
+  }
+};
+
+SensorThread flowSensor = SensorThread();
+SensorThread pressureSensor = SensorThread();
+
+ThreadController controller = ThreadController();
+
+DynamicJsonDocument doc(1024);
+
+// -------------------------------------------------------------------------------------------------------- init timer
+void timerInit()
+{
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = 800; // compare match register 16MHz/256/2Hz sementara 25
+  // TCCR1B |= (1 << WGM12);   // CTC mode
+  // TCCR1B |= (1 << CS01);    // 64 prescaler
+  TCCR1B = 0;
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+
+  sei();
+}
+// -------------------------------------------------------------------------------------------------------- init pin
+void initPin()
+{
+  pinMode(LIMITSWITCH_L, INPUT);
+  pinMode(LIMITSWITCH_R, INPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(PULSE_PIN, OUTPUT);
+  pinMode(EN_PIN, OUTPUT);
+
+  digitalWrite(LIMITSWITCH_L, HIGH);
+  digitalWrite(LIMITSWITCH_R, HIGH);
+  digitalWrite(DIR_PIN, LOW);
+  digitalWrite(PULSE_PIN, LOW);
+  digitalWrite(EN_PIN, HIGH);
+
+  pinMode(13, 1);
+}
+
+void dir_out()
+{
+  digitalWrite(DIR_PIN, HIGH); // Gerak keluar
+}
+
+void dir_in()
+{
+  digitalWrite(DIR_PIN, LOW); // gerak kedalam
+}
+
+void enable()
+{
+  digitalWrite(EN_PIN, HIGH);
+}
+
+void disable()
+{
+  digitalWrite(EN_PIN, LOW);
+}
+
+void calibratePosition()
+{
+  dir_out();
+  OCR1A = 400;
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS01);  // 64 prescaler
+  while (1)
+  {
+    /* code */
+    if (digitalRead(LIMITSWITCH_R) == LOW)
+    {
+      TCCR1B = 0;
+      OCR1A = 800;
+      break;
+    }
+
+    delay(1);
+  }
+}
+
+bool push = false;
+void step_on()
+{
+  push = true;
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS01);  // 64 prescaler
+}
+
+void phaseCallback()
+{
+  if (MACHNE_ON == false)
+  {
+    return;
+  }
+  if (phase == 0)
+  {
+    // Serial.println("phase 0");
+    dir_in();
+    step_on();
+    phase++;
+    delay(250);
+    push = true;
+  }
+  else if (phase == 1)
+  {
+    // Serial.println("phase 1");
+    dir_out();
+    step_on();
+    phase++;
+    delay(50);
+    push = true;
+  }
+  else if (phase >= expiration)
+  {
+    // Serial.println("reset");
+    phase = 0;
+  }
+  else
+  {
+    phase++;
+  }
+}
+
+float adc2cmh2o(float adc)
+{
+  float cmh2o;
+  float gain = 100.0 / 1000.0;
+  const float kPa2cmH2O = 10.19716;
+
+  cmh2o = kPa2cmH2O * (((adc / 1023.0 * 5.0) / gain) / 2.5);
+  // cmh2o = (((adc/1023.0f)*5)/gain)/2.5;
+  return cmh2o;
+}
+
+void staticDispCallback()
+{
+  if (SENDDATA == false)
+    return;
+
+  // Serial.print(77);
+  // Serial.print(",");
+  // Serial.print(disp_volume);
+  // Serial.print(",");
+  // Serial.print(disp_oxygen);
+  // Serial.print(",");
+  // Serial.print(disp_flow);
+  // Serial.println();
+  Serial.print(77);
+  Serial.print(",");
+  Serial.print(rand() % 100 + 1);
+  Serial.print(",");
+  Serial.print((adc2cmh2o(pressureSensor.current_output) - 26.70) * 2);
+  Serial.print(",");
+  Serial.print(rand() % 100 + 1);
+  Serial.println();
+}
+
+void calibrate()
+{
+  unsigned long dump = 0;
+  for (int x = 0; x <= 1000; x++)
+  {
+    dump += adc2cmh2o(pressureSensor.current_output);
+    Serial.println(dump);
+    delay(1);
+  }
+  offset_pressure = dump / 1000;
+  Serial.println(offset_pressure);
+  delay(2000);
+}
+void sendDataCallback()
+{
+  if (SENDDATA == false)
+  {
+    return;
+  }
+  // Serial.print("FL:");
+  // // Serial.print(map(flowSensor.current_output,0,88,0,2));
+  // Serial.print(pressureSensor.current_output);
+  // Serial.print("\tPS:");
+  // // Serial.print(map(pressureSensor.current_output, 0, 560, 0, 45));
+  // Serial.print(adc2cmh2o(pressureSensor.current_output),6);
+  // Serial.print(pressureSensor.current_output);
+  // Serial.print("\t:");
+  // Serial.print(millis());
+
+  Serial.print("59");
+  Serial.print(",");
+  Serial.print(125.0f);
+  Serial.print(",");
+  Serial.print(flowSensor.current_output);
+  Serial.print(",");
+  Serial.print((adc2cmh2o(pressureSensor.current_output) - 26.70) * 2);
+
+  Serial.println();
+}
+
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    serial_text = Serial.readStringUntil('\n');
+    deserializeJson(doc, serial_text);
+
+    String command = doc["c"];
+    if (command == "start")
+    {
+      if (MACHNE_ON == true)
+        return;
+      SENDDATA = true;
+      Serial.println("Mesin Nyala");
+      phaseThread.setInterval(60000.0f / ((inspiration + expiration) * breath_frequency));
+      calibratePosition();
+      phase = 0;
+      MACHNE_ON = true;
+    }
+    else if (command == "stop")
+    {
+      MACHNE_ON = false;
+      SENDDATA = false;
+      TCCR1A = 0;
+      calibratePosition();
+      Serial.println("Mesin Mati");
+    }
+    else if (command == "set")
+    {
+      expiration = doc["ie"];
+      breath_frequency = doc["f"];
+    }
+  }
+}
+
+// -------------------------------------------------------------------------------------------------------- setup program
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial.println("Prepairing!");
+  initPin();
+  Serial.println("Pin init done!");
+  timerInit();
+  Serial.println("Timer init done!");
+  calibratePosition();
+  delay(100);
+  Serial.println("Calibrate position done!");
+
+  Serial.println("Done!");
+  // SENDDATA = true;
+#ifndef ESP8266
+  while (!Serial)
+    ; // wait for serial port to connect. Needed for native USB
+#endif
+
+  // if (! rtc.begin()) {
+  //   Serial.println("Couldn't find RTC");
+  //   Serial.flush();
+  //   abort();
+  // }
+
+  if (!rtc.isrunning())
+  {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // put your setup code here, to run once:
+  flowSensor.pin = FLOWPIN;
+  pressureSensor.pin = PRESSUREPIN;
+  flowSensor.setInterval(100);
+  pressureSensor.setInterval(100);
+  flowSensor.alpha = 0.15;
+  pressureSensor.alpha = 0.15;
+
+  controller.add(&sendData);
+  controller.add(&flowSensor);
+  controller.add(&pressureSensor);
+
+  phaseThread.onRun(phaseCallback);
+  phaseThread.setInterval(1500);
+  sendData.onRun(sendDataCallback);
+  sendData.setInterval(10);
+  sendStaticData.onRun(staticDispCallback);
+  sendStaticData.setInterval(500);
+  calibrate();
+  SENDDATA = true;
+}
+// -------------------------------------------------------------------------------------------------------- loop program
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  // DateTime time = rtc.now();
+  if (phaseThread.shouldRun())
+  {
+    phaseThread.run();
+  }
+  controller.run();
+  // Serial.println("aaaa");
+  // Serial.println("cccc");
+
+  if (push)
+  {
+    // Serial.println("bbbb");
+    if ((digitalRead(LIMITSWITCH_R) == LOW) || (digitalRead(LIMITSWITCH_L) == LOW))
+    {
+      push = false;
+      TCCR1B = 0;
+    }
+  }
+}
+
+// -------------------------------------------------------------------------------------------------------- ISR TIMER 1 COMPA
+ISR(TIMER1_COMPA_vect) // timer compare interrupt service routine
+{
+  digitalWrite(PULSE_PIN, digitalRead(PULSE_PIN) ^ 1); // toggle LED pin
+}
